8df21c39efbc7ef28786ea012ada2b345e725db5
JayMackay <jared.mackay1994@live.com>
2020-01-31 21:08:21 +0000

    Calculator Input & Operations

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Console_Calculator
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a number: ");
+            double inputOne = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please input an operator: ");
+            string function = Console.ReadLine();
+
+            Console.Write("Please input a number: ");
+            double inputTwo = Convert.ToDouble(Console.ReadLine());
+
+            if(function == "+")
+            {
+                Console.WriteLine(inputOne + inputTwo);
+            } 
+            
+            else if(function == "-")
+            {
+                Console.WriteLine(inputOne - inputTwo);
+            }
+
+            else if (function == "/")
+            {
+                Console.WriteLine(inputOne / inputTwo);
+            }
+
+            else if (function == "*")
+            {
+                Console.WriteLine(inputOne * inputTwo);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
